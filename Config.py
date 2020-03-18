ac48c278438ca0431b6c58082690c55ee67fd6e1
Shahar <shaharluftig@gmail.com>
2020-03-18 15:39:49 +0200

    Cleaned some code

--- Config.py
+++ Config.py
@@ -1,7 +1,10 @@
-JARS = "./Jars/delta-core_2.11-0.5.0.jar," \
-       "./Jars/spark-sql-kafka-0-10_2.11-2.4.5.jar," \
-       "./Jars/kafka-clients-2.4.1.jar"
-
+# KAFKA Config
 TOPICS = "test"
 SERVERS = "localhost:9092"
 PATH = "C:/Users/shaha/Desktop/Projects/ElasticQSM/HdfsElasticQSM/try_index"
+
+# SPARK Config
+JARS = "./Jars/delta-core_2.11-0.5.0.jar," \
+       "./Jars/spark-sql-kafka-0-10_2.11-2.4.5.jar," \
+       "./Jars/kafka-clients-2.4.1.jar"
+APP_NAME = "DeltaKafkaWriter"
--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 from pyspark.sql import SparkSession
 
-from Config import JARS
+from Config import JARS, APP_NAME
 from Workflow import execute_workflow
 
 if __name__ == '__main__':
-    context = SparkSession.builder.master("local").config("spark.jars", JARS).appName("DeltaKafkaWriter").getOrCreate()
+    context = SparkSession.builder.master("local").config("spark.jars", JARS).appName(APP_NAME).getOrCreate()
     execute_workflow(context)
-    print("Done")
